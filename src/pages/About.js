f657b70da7975ed68f7d4125af13f5243a2ebe30
Kingsley Atuba <atubak40@gmail.com>
2021-03-02 09:35:42 +0100

    good

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel illo, asperiores nostrum sapiente qui
+				ducimus amet voluptatibus repudiandae adipisci inventore voluptas voluptate voluptates, tempora rerum
+				doloribus accusantium repellat delectus optio nihil quibusdam perspiciatis quam tempore repellendus
+				molestiae. Pariatur, aliquid saepe.
+			</p>
+		</section>
+	)
+}
+
+export default About
